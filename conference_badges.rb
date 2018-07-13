01fcc3b09ec5013693f6653b211bb8f8721a185f
Karla R <karla.y.reyes12@gmail.com>
2018-07-13 07:59:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
+def batch_badge_creator(attendees)
  badge_messages = []
- names.each {|x| return "Hello, my name is #{x}."}
+ names.each do |attendee|
+   message = badge_maker(attendee)
+   badge_messages << message
 end
